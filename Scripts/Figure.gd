1f7a0032975e4a62772b161e0a8b39fa28340db4
neclor <al.pavlov2012@gmail.com>
2023-06-16 15:58:26 +0200

    Add rotation

--- Scripts/Figure.gd
+++ Scripts/Figure.gd
@@ -1,53 +1,57 @@
-class_name Figure extends Node
-
-const block_size = 32
-
-const dictionary = {
-	"i": [Color("#00ffff"), Vector2(-block_size, 0), Vector2(0, 0), Vector2(block_size, 0), Vector2(block_size * 2, 0)],
-	"o": [Color("#ffff"), Vector2(0, -block_size), Vector2(block_size, -block_size), Vector2(0, 0), Vector2(block_size, 0)],
-	"t": [Color("#ff00ff"), Vector2(0, -block_size), Vector2(-block_size, 0), Vector2(0, 0), Vector2(block_size, 0)],
-	"j": [Color("#ff7f00"), Vector2(-block_size, -block_size), Vector2(-block_size, 0), Vector2(0, 0), Vector2(block_size, 0)],
-	"l": [Color("#0000ff"), Vector2(block_size, -block_size), Vector2(-block_size, 0), Vector2(0, 0), Vector2(block_size, 0)],
-	"s": [Color("#00ff00"), Vector2(0, -block_size), Vector2(block_size, -block_size), Vector2(-block_size, 0), Vector2(0, 0)],
-	"z": [ Color("#ff0000"), Vector2(-block_size, -block_size), Vector2(0, -block_size), Vector2(0, 0), Vector2(block_size, 0)],
-}
+extends Node
+class_name Figure
 
 var top_border
 var bottom_border
 var left_border
 var right_border
 
+const figures_description = {
+	"i": [Color("#00ffff"), Vector2(-Block.block_size, 0), Vector2(0, 0), Vector2(Block.block_size, 0), Vector2(Block.block_size * 2, 0)],
+	"o": [Color("#ffff00"), Vector2(0, -Block.block_size), Vector2(Block.block_size, -Block.block_size), Vector2(0, 0), Vector2(Block.block_size, 0)],
+	"t": [Color("#ff00ff"), Vector2(0, -Block.block_size), Vector2(-Block.block_size, 0), Vector2(0, 0), Vector2(Block.block_size, 0)],
+	"j": [Color("#ff7f00"), Vector2(-Block.block_size, -Block.block_size), Vector2(-Block.block_size, 0), Vector2(0, 0), Vector2(Block.block_size, 0)],
+	"l": [Color("#0000ff"), Vector2(Block.block_size, -Block.block_size), Vector2(-Block.block_size, 0), Vector2(0, 0), Vector2(Block.block_size, 0)],
+	"s": [Color("#00ff00"), Vector2(0, -Block.block_size), Vector2(Block.block_size, -Block.block_size), Vector2(-Block.block_size, 0), Vector2(0, 0)],
+	"z": [ Color("#ff0000"), Vector2(-Block.block_size, -Block.block_size), Vector2(0, -Block.block_size), Vector2(0, 0), Vector2(Block.block_size, 0)],
+}
+
 var coordinates
 var blocks_coordinates
-@export var blocks: Array = []
+var blocks
 
-var figures_bag
-
-func _init(set_top_border, set_bottom_border, set_left_border, set_right_border, initial_coordinates, figure_name):
+func _init(figure_name, initial_coordinates, set_top_border, set_bottom_border, set_left_border, set_right_border):
 	top_border = set_top_border
 	bottom_border = set_bottom_border
 	left_border = set_left_border
 	right_border = set_right_border
 
 	coordinates = initial_coordinates
-	var structure = dictionary[figure_name];
-	var color = structure[0]
-	blocks_coordinates = structure.slice(1)
 
-	for i in len(structure) - 1:
+	var figure_description = figures_description[figure_name];
+
+	blocks_coordinates = figure_description.slice(1)
+
+	var color = figure_description[0]
+
+	blocks = []
+
+	for i in len(figure_description) - 1:
 		var block = Block.Block_scene.instantiate()
 		var block_sprite = block.get_node("Block")
-		Block.change_color(block_sprite, color, color)
-		blocks.append(block)
-	update_blocks_coordinates()
 
+		Block.change_color(block_sprite, color, color)
+
+		blocks.append(block)
+
+	update_blocks_coordinates()
 
 func update_blocks_coordinates():
 	for i in len(blocks):
 		blocks[i].position = blocks_coordinates[i] + coordinates
 
 func check_move_down(locked_blocks):
-	if blocks == []:
+	if blocks == []: #or paused
 		return null
 
 	else:
@@ -56,13 +60,13 @@ func check_move_down(locked_blocks):
 		for block in locked_blocks:
 			locked_blocks_coordinates.append(block.position)
 
-		var next_coordinates_block
+		var next_block_coordinates
 
 		for block in blocks:
-			next_coordinates_block = block.position
-			next_coordinates_block.y += Block.block_size
+			next_block_coordinates = block.position
+			next_block_coordinates.y += Block.block_size
 
-			if next_coordinates_block.y > bottom_border or next_coordinates_block in locked_blocks_coordinates:
+			if next_block_coordinates.y > bottom_border or next_block_coordinates in locked_blocks_coordinates:
 				return false
 
 		move_down()
@@ -71,3 +75,91 @@ func check_move_down(locked_blocks):
 func move_down():
 	coordinates.y += Block.block_size
 	update_blocks_coordinates()
+
+func check_move_rotation(locked_blocks):
+	if blocks == []: #or paused
+		return null
+
+	else:
+		var locked_blocks_coordinates = []
+
+		for block in locked_blocks:
+			locked_blocks_coordinates.append(block.position)
+
+		var next_block_coordinates
+
+		for block_coordinates in blocks_coordinates:
+			next_block_coordinates = Vector2(-block_coordinates.y, block_coordinates.x) + coordinates
+
+			if next_block_coordinates.y > bottom_border or next_block_coordinates.x > right_border or next_block_coordinates.x < left_border or next_block_coordinates in locked_blocks_coordinates:
+				return false
+
+		rotation()
+		return true
+
+func rotation():
+	var next_block_coordinates
+
+	for i in len(blocks_coordinates):
+		next_block_coordinates = blocks_coordinates[i].x
+		blocks_coordinates[i].x = -blocks_coordinates[i].y
+		blocks_coordinates[i].y = next_block_coordinates
+
+	update_blocks_coordinates()
+
+func check_move_right(locked_blocks):
+	if blocks == []: #or paused
+		return null
+
+	else:
+		var locked_blocks_coordinates = []
+
+		for block in locked_blocks:
+			locked_blocks_coordinates.append(block.position)
+
+		var next_block_coordinates
+
+		for block in blocks:
+			next_block_coordinates = block.position
+			next_block_coordinates.x += Block.block_size
+
+			if next_block_coordinates.x > right_border or next_block_coordinates in locked_blocks_coordinates:
+				return false
+
+		move_right()
+		return true
+
+func move_right():
+	coordinates.x += Block.block_size
+	update_blocks_coordinates()
+
+func check_move_left(locked_blocks):
+	if blocks == []: #or paused
+		return null
+
+	else:
+		var locked_blocks_coordinates = []
+
+		for block in locked_blocks:
+			locked_blocks_coordinates.append(block.position)
+
+		var next_block_coordinates
+
+		for block in blocks:
+			next_block_coordinates = block.position
+			next_block_coordinates.x -= Block.block_size
+
+			if next_block_coordinates.x < left_border or next_block_coordinates in locked_blocks_coordinates:
+				return false
+
+		move_left()
+		return true
+
+func move_left():
+	coordinates.x -= Block.block_size
+	update_blocks_coordinates()
+
+
+
+
+
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,23 +1,21 @@
 extends Node2D
 
-const block_size = 32
-
 const field_height = 22
 const field_width = 10
 
 const top_border = -(field_height / 2) * Block.block_size
 const bottom_border = (field_height / 2 - 1) * Block.block_size
 const left_border = -(field_width / 2) * Block.block_size
-const right_border =(field_width / 2) * Block.block_size
+const right_border =(field_width / 2 - 1) * Block.block_size
 
 const initial_coordinates = Vector2(-Block.block_size, -(field_height / 2 - 1) * Block.block_size)
 
 const figure_names = ["i", "o", "t", "j", "l", "s", "z"]
 
-var structure_bag = []
-var next_name
+var figure_names_bag
+var next_figure_name
 
-var locked_blocks = []
+var locked_blocks
 
 var figure
 #var figure_ghost
@@ -27,29 +25,72 @@ func _ready():
 	init()
 
 func init():
-	chose_next_structure()
-	add_new_figure_to_field()
+	figure_names_bag = []
+	locked_blocks = []
+
+	chose_next_figure()
+	creating_new_figure()
 
 func _on_game_timer_timeout():
 	if !figure.check_move_down(locked_blocks):
 		locked_blocks.append_array(figure.blocks)
 
-		add_new_figure_to_field()
+		creating_new_figure()
 
-
-func add_new_figure_to_field():
-	figure = Figure.new(top_border, bottom_border, left_border, right_border, initial_coordinates, chose_next_structure())
+func creating_new_figure():
+	figure = Figure.new(chose_next_figure(), initial_coordinates, top_border, bottom_border, left_border, right_border)
 	for block in figure.blocks:
 		$Field.add_child(block)
 
-func chose_next_structure():
-	if structure_bag == []:
-		structure_bag = figure_names.duplicate()
+func chose_next_figure():
+	if figure_names_bag == []:
+		figure_names_bag = figure_names.duplicate()
 
-	var new_structure_index = randi_range(0, len(structure_bag) - 1)
-	var current_name = next_name
+	var new_figure_name_index = randi_range(0, len(figure_names_bag) - 1)
+	var current_figure_name = next_figure_name
 
-	next_name = structure_bag[new_structure_index]
-	structure_bag.remove_at(new_structure_index)
+	next_figure_name = figure_names_bag[new_figure_name_index]
+	figure_names_bag.remove_at(new_figure_name_index)
 
-	return current_name
+	return current_figure_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func _input(event):
+	if Input.is_action_pressed("Down"):
+		figure.check_move_down(locked_blocks)
+
+	if Input.is_action_pressed("Right"):
+		figure.check_move_right(locked_blocks)
+
+	if Input.is_action_pressed("Left"):
+		figure.check_move_left(locked_blocks)
+
+	if Input.is_action_just_pressed("Rotation"):
+		figure.check_move_rotation(locked_blocks)
+
+
+
+
+
+
+
+
