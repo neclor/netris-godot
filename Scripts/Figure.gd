7cdd7f14e2e8f383e33da624e077391920edc29d
ufasoft <dev@ufasoft.com>
2023-06-15 17:11:21 -0700

    Use Dictionary for figure forms

--- Scripts/Figure.gd
+++ Scripts/Figure.gd
@@ -1,5 +1,17 @@
 class_name Figure extends Node
 
+const block_size = 32
+
+const dictionary = {
+	"i": [Color("#00ffff"), Vector2(-block_size, 0), Vector2(0, 0), Vector2(block_size, 0), Vector2(block_size * 2, 0)],
+	"o": [Color("#ffff"), Vector2(0, -block_size), Vector2(block_size, -block_size), Vector2(0, 0), Vector2(block_size, 0)],
+	"t": [Color("#ff00ff"), Vector2(0, -block_size), Vector2(-block_size, 0), Vector2(0, 0), Vector2(block_size, 0)],
+	"j": [Color("#ff7f00"), Vector2(-block_size, -block_size), Vector2(-block_size, 0), Vector2(0, 0), Vector2(block_size, 0)],
+	"l": [Color("#0000ff"), Vector2(block_size, -block_size), Vector2(-block_size, 0), Vector2(0, 0), Vector2(block_size, 0)],
+	"s": [Color("#00ff00"), Vector2(0, -block_size), Vector2(block_size, -block_size), Vector2(-block_size, 0), Vector2(0, 0)],
+	"z": [ Color("#ff0000"), Vector2(-block_size, -block_size), Vector2(0, -block_size), Vector2(0, 0), Vector2(block_size, 0)],
+}
+
 var top_border
 var bottom_border
 var left_border
@@ -11,19 +23,21 @@ var blocks_coordinates
 
 var figures_bag
 
-func _init(set_top_border, set_bottom_border, set_left_border, set_right_border, initial_coordinates, structure):
+func _init(set_top_border, set_bottom_border, set_left_border, set_right_border, initial_coordinates, figure_name):
 	top_border = set_top_border
 	bottom_border = set_bottom_border
 	left_border = set_left_border
 	right_border = set_right_border
 
 	coordinates = initial_coordinates
-	blocks_coordinates = structure.slice(2).duplicate()
+	var structure = dictionary[figure_name];
+	var color = structure[0]
+	blocks_coordinates = structure.slice(1)
 
-	for i in len(structure) - 2:
+	for i in len(structure) - 1:
 		var block = Block.Block_scene.instantiate()
 		var block_sprite = block.get_node("Block")
-		Block.change_color(block_sprite, structure[1], structure[1])
+		Block.change_color(block_sprite, color, color)
 		blocks.append(block)
 	update_blocks_coordinates()
 
